1a7dd1ff1d22983531881dc1ab0373b5431665c6
Joel Crawford <joelcrawford30@gmail.com>
2023-10-05 15:25:16 +0300

    Counting Palindrome

---
+++ CountingPalindromes.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+
+//func findPallinDromes(sentence: String) -> Int {
+//    return sentence.split{$0==" "}.map(String.init).filter{String($0) == String(String($0).reversed())}.count
+//}
+
+var greeting = "Hello, playground"
+
+
+func allPalindromes(sentence: String) -> [String: Int] {
+    var counts = [String:Int]()
+    
+    let words = sentence.components(separatedBy: " ")
+    
+    words.forEach { word in
+        if isPalindrome(word: word.lowercased()) {
+            let count =  counts[word] ?? 0
+            counts[word] = count + 1
+            
+        }
+    }
+    return counts
+}
+
+
+fileprivate func isPalindrome(word: String) -> Bool {
+    
+    let characters = Array(word)
+    
+    var currentIndex = 0
+    var midPointIndex = Int(characters.count / 2)
+    
+    while currentIndex < midPointIndex {
+        
+        //Check if charcters are equal
+        if(characters[currentIndex] != characters[characters.count - currentIndex - 1]){
+            return false
+        }
+        currentIndex += 1
+    }
+    
+    return true
+    
+}
+
+let sentence = "madam anna kayak notpalindrome anna"
+
+print(allPalindromes(sentence: sentence))
