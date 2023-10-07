a73092f8128bfeeba753572ff41ae7eebb889fe0
Joel Crawford <joelcrawford30@gmail.com>
2023-10-07 16:21:01 +0300

    Cleaned Code

--- MostCommonItems.playground/Contents.swift
+++ MostCommonItems.playground/Contents.swift
@@ -7,26 +7,50 @@ func getMostCommonColor(array: [String]) -> [String?] {
     var colorDictionary: [String: Int] = [:]
     
     for color in array {
-        if let count  = colorDictionary[color]{
+        if let count = colorDictionary[color] {
             colorDictionary[color] = count + 1
-        }else{
-            //No count
-            colorDictionary[color] =  1
+        } else {
+            colorDictionary[color] = 1
         }
-        
-        //iterate over dictionary
-        let highestValue = colorDictionary.values.max()
-        
-        for (color, count) in colorDictionary {
-            
-            if(colorDictionary[color] == highestValue){
-                topColors.append(color)
-            }
+    }
+    
+    let highestValue = colorDictionary.values.max()
+    
+    for (color, count) in colorDictionary {
+        if count == highestValue {
+            topColors.append(color)
         }
     }
     
     return topColors
-    
 }
+
+
+//func getMostCommonColor(array: [String]) -> [String?] {
+//    var topColors: [String] = []
+//    var colorDictionary: [String: Int] = [:]
+//
+//    for color in array {
+//        if let count  = colorDictionary[color]{
+//            colorDictionary[color] = count + 1
+//        }else{
+//            //No count
+//            colorDictionary[color] =  1
+//        }
+//
+//        //iterate over dictionary
+//        let highestValue = colorDictionary.values.max()
+//
+//        for (color, count) in colorDictionary {
+//
+//            if(colorDictionary[color] == highestValue){
+//                topColors.append(color)
+//            }
+//        }
+//    }
+//
+//    return topColors
+//
+//}
 
 getMostCommonColor(array: colorArray)
